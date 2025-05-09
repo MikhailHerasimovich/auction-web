7b901d6b790a320eb6010b4c6ef1c212a08b7091
Mihail Gerasimovich <gerasimovich.mihail.pro@gmail.com>
2025-05-09 15:40:53 +0300

    fix: remove file storage test files

--- src/app/(auth)/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import { googleStorageClient } from '@/lib/google-storage.client';
-
-export default async function LoginPage() {
-  const buckets = await googleStorageClient.getBuckets();
-
-  return <div>Login {JSON.stringify(buckets || {})}</div>;
-}
